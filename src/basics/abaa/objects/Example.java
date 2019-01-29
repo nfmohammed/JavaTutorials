afc0aed14868f7c292fd7b4d0f8fcf4a2edee352
Naseeruddin Feroz Mohammed <nmohammed@expedia.com>
2019-01-29 17:14:27 -0600

    redo object example

---
+++ src/basics/abaa/objects/Example.java
@@ -0,0 +1,23 @@
+package basics.abaa.objects;
+
+public class Example {
+    public static void main(String[] args) {
+        Person john = new Person("John", 30);
+        john.printName();
+
+        Person james = new Person("James", 25);
+        james.printName();
+    }
+}
+
+class Person {
+    private String name;
+    private Integer age;
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void printName() {
+        System.out.println("Name = " + this.name+ ", Age = " + this.age);
+    }
+}
